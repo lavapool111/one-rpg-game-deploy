aaca0fa05ff43dce561eab27408ef03db7dcf158
brocumfriends <brocumfriends@gmail.com>
2026-01-17 10:35:25 -0500

    mobile addition

--- components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-// UI Components - HUD, menus, and interface elements
-
-export { HUD } from './HUD';
-export { PlayerHUD } from './PlayerHUD';
-export { MainMenu } from './MainMenu';
-export { PauseMenu } from './PauseMenu';
-export { DeathScreen } from './DeathScreen';
-export { GameUI } from './GameUI';
-export { InventoryScreen } from './InventoryScreen';
--- lib/game/index.ts
+++
@@ -1,6 +0,0 @@
-// Game Logic - Combat, enemies, scaling, and core mechanics
-
-export * from './combat';
-export * from './enemies';
-export * from './scaling';
-export * from './pillars';
